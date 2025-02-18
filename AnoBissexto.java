3725376e115f5ae9ec1c6070c2467f363a4676e1
Melo de Morais <fabiano.melodemorais@thomsonreuters.com>
2025-02-18 08:16:05 -0300

    aprendendoGit

---
+++ AnoBissexto.java
@@ -0,0 +1,14 @@
+package OperadoresRelacionais;
+
+import java.util.Scanner;
+
+public class AnoBissexto {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um ano: ");
+        int ano = scanner.nextInt();
+
+        if ((ano % == 0 && ano % 100 != 0) || (ano % 400 == 0))
+    }
+}
